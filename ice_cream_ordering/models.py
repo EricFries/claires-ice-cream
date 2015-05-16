b2a22064268392b8d581e053c5fe757cfaf3a2b7
EricFries <ericfries@gmail.com>
2015-05-16 13:41:56 -0400

    add icecream and topping models

--- ice_cream_ordering/models.py
+++ ice_cream_ordering/models.py
@@ -3,8 +3,19 @@ from django.db import models
 # Create your models here.
 
 class Order(models.Model):
-    order_name = models.CharField(max_length=30)
-    order_address = models.CharField(max_length=30)
-    order_phone = models.CharField(max_length=30)
-    order_email = models.CharField(max_length=30)
-    order_date = models.DateTimeField('date ordered')
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=75)
+    phone = models.CharField(max_length=12)
+    email = models.CharField(max_length=30)
+    date = models.DateTimeField('date ordered')
+
+    def __str__(self):
+        return self.order_name
+
+class IceCream(models.Model):
+    order = models.ForeignKey(Order)
+    flavor = models.CharField(max_length=30)
+
+class Topping(models.Model):
+    order = models.ForeignKey(Order)
+    variety = models.CharField(max_length=30)
