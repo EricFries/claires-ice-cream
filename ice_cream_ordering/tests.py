6250c79b5891b619fe5266fcf18545e418a23da4
EricFries <ericfries@gmail.com>
2015-05-19 20:02:56 -0400

    add test for order model

--- ice_cream_ordering/tests.py
+++ ice_cream_ordering/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from .models import Order,IceCream,Topping,Container
+from django.utils import timezone
 # Create your tests here.
+
+class OrderTestCase(TestCase):
+    def setUp(self):
+        Order.objects.create(name="Eric", address="11 Broadway, Ste. 200, New York, NY 10010", phone="555-555-5555", email="eric@eric.com", date=timezone.now())
+
+    def test_order_has_name(self):
+        eric = Order.objects.get(name="Eric")
+        self.assertEqual(eric.name, 'Eric')
